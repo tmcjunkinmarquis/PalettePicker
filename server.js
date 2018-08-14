90e9d5bb5b7fc13ef9a71a0050d7c696ac72a55f
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-14 15:06:19 -0600

    Add locals for projects and palettes in server.js

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.get('/', (request, response) => {
   // response.status(200).sendFile('public/index.html', { root: __dirname });
 });
 
+app.locals.projects = [
+  { id: 'p1', name: 'Hello World' },
+  { id: 'p2', name: 'Goodbye World' },
+  { id: 'p3', name: 'So Sad' }
+];
+
+app.locals.palettes = [
+  { id: 'l1', project_id: 'p1', color_one: '#3BABCB', color_two: '#3BABCB', color_three: '#3BABCB', color_four: '#3BABCB', color_five: '#3BABCB'},
+  { id: 'l2', project_id: 'p2', color_one: '#2AE13B', color_two: '#2AE13B', color_three: '#2AE13B', color_four: '#2AE13B', color_five: '#2AE13B' },
+  { id: 'l3', project_id: 'p3', color_one: '#D73479', color_two: '#D73479', color_three: '#D73479', color_four: '#D73479', color_five: '#D73479' }
+];
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
