906a74a486efe008425cbbaa0000650d59ed55c2
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-15 08:49:11 -0600

    Add saveProjectToOption function

--- public/js/app.js
+++ public/js/app.js
@@ -1,62 +1,73 @@
-// $().ready(()=>{
-  // const projects = getProjects();
-  // const palettes = getPalettes();
-
-// });
-
 $(document).ready(function () {
+  
 
-  function makeRandomColors() {
-    
+  
+  function getPalettes() {
+    const myRequest = new Request('http://localhost:3000/api/v1/palettes', { method: 'GET'});
+    fetch(myRequest)
+    .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong on api server!');
+        }
+      })
+      .then(response => {
+        console.debug(response);
+        // ...
+      }).catch(error => {
+        console.error(error);
+      });
   }
 
-  function handleClick(event) {
-    
+  function getProjects (){
+    const myRequest = new Request('http://localhost:3000/api/v1/projects', { method: 'GET' });
+    const promise = fetch(myRequest)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong on api server!');
+      }
+    })
+    .then(response => {
+      console.debug(response);
+      // ...
+    }).catch(error => {
+      console.error(error);
+    });
+    return promise;
+  }
+  
+  let projectsData = getProjects()
+  let projects = projectsData.then(function(result) {
+    console.log(result);
+    ;
+  });
+  console.log(projects);
+  
+  let palettesData = getPalettes();
+
+
+  function makeRandomColors() {
+      var randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
+      return randomColor;
+  }
+
+  function handleClick(event) { 
     event.preventDefault();
-    return $('.circle').each(function () {
-      
-      $(this).css('background-color', function() {
-        var randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
-        
-        return randomColor
-        // const upperColor = color.toUpperCase()
-        // return upperColor;
-      });
+    return $('.circle').each(function () { 
+      $(this).css('background-color', makeRandomColors());
     });
   }
 
-  
-
   $('.different-palette').on('click', handleClick);
 
-  // $.fn.redraw = function () {
-  //   $(this).each(function () {
-  //     var redraw = this.offsetHeight;
-  //   });
-  // };
-  // You'd then call the method like this:
+  function saveProjectToOption() {
+    
+  }
 
-  // $('.theElement').redraw();
+  $(saveProjectToOption);
+
 });
-
-
-
-  
-  
-// const getProjects = () => {
-//   fetch('http://localhost:3000/api/v1/projects').then(response => {
-//     response.json().then(json => {
-//       let projectsData = json;
-//     });
-//   });
-// };
-
-
-  
-
-//   const getPalettes = async () => {
-//     const url = 'http://localhost:3000/api/v1/palettes'
-//     const response = await fetch(url);
-//     const palettesData = await response.json();
-//     return palettesData;
-//   }
+ 
