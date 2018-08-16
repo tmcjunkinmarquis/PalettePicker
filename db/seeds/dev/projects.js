1a81e45de5021a5fa2db2ec38fd35655550c5778
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-16 16:31:21 -0600

    Add gett requests to server for projects and palettes and confirm with postman

---
+++ db/seeds/dev/projects.js
@@ -0,0 +1,20 @@
+
+exports.seed = function (knex, Promise) {
+  return knex('palettes').del() 
+    .then(() => knex('projects').del()) 
+    .then(() => {
+      return Promise.all([
+        knex('projects').insert({
+          project_name: 'First Project'}, 'id')
+          .then(project => {
+            return knex('palettes').insert([
+              { palette_name: 'Pretty', project_id: project[0], color_one: '#f89d64', color_two: '#60ec83', color_three: '#31782c', color_four: '#50efc0', color_five:'#418282' },
+              { palette_name: 'Ugly', project_id: project[1], color_one: '#25e635', color_two: '#1ece27', color_three: '#a32380', color_four: '#1cddb0', color_five: '#396358' }
+            ]);
+          })
+          .then(() => console.log('Seeding complete!'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
+      ]); 
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
@@ -10,28 +13,36 @@ app.get('/', (request, response) => {
   // response.status(200).sendFile('public/index.html', { root: __dirname });
 });
 
-app.locals.projects = [
-  { id: 'p1', name: 'Hello World' },
-  { id: 'p2', name: 'Goodbye World' },
-  { id: 'p3', name: 'So Sad' }
-];
+// app.locals.projects = [
+//   { id: 'p1', name: 'Hello World' },
+//   { id: 'p2', name: 'Goodbye World' },
+//   { id: 'p3', name: 'So Sad' }
+// ];
 
-app.locals.palettes = [
-  { id: 'l1', project_id: 'p1', color_one: '#3BABCB', color_two: '#3BABCB', color_three: '#3BABCB', color_four: '#3BABCB', color_five: '#3BABCB'},
-  { id: 'l2', project_id: 'p2', color_one: '#2AE13B', color_two: '#2AE13B', color_three: '#2AE13B', color_four: '#2AE13B', color_five: '#2AE13B' },
-  { id: 'l3', project_id: 'p3', color_one: '#D73479', color_two: '#D73479', color_three: '#D73479', color_four: '#D73479', color_five: '#D73479' }
-];
+// app.locals.palettes = [
+//   { id: 'l1', project_id: 'p1', color_one: '#3BABCB', color_two: '#3BABCB', color_three: '#3BABCB', color_four: '#3BABCB', color_five: '#3BABCB'},
+//   { id: 'l2', project_id: 'p2', color_one: '#2AE13B', color_two: '#2AE13B', color_three: '#2AE13B', color_four: '#2AE13B', color_five: '#2AE13B' },
+//   { id: 'l3', project_id: 'p3', color_one: '#D73479', color_two: '#D73479', color_three: '#D73479', color_four: '#D73479', color_five: '#D73479' }
+// ];
 
 app.get('/api/v1/projects', (request, response) => {
-  const projects = app.locals.projects;
-
-  response.json({ projects });
+  database('projects').select()
+  .then((projects)=>{
+    response.status(200).json(projects);
+  })
+  .catch((error)=>{
+    response.status(500).json({ error });
+  });
 });
 
 app.get('/api/v1/palettes', (request, response) => {
-  const palettes = app.locals.palettes;
-
-  response.json({ palettes });
+  database('palettes').select()
+    .then((palettes) => {
+      response.status(200).json(palettes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
