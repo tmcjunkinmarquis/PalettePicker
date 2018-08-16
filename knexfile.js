0794858b9c572249f75de81d3cbc190868558c43
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-16 15:57:07 -0600

    Add migrations and seed directory

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,14 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/palettepicker',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
-  },
+  }
 
   // production: {
   //   client: 'postgresql',
