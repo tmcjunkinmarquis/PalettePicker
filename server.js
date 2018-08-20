96e0fc1f37149e6f3afb5bc8ee81a71808c3f902
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-20 11:41:32 -0600

    Add some annotations to server file

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const app = express();
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
-const bodyParser = require('body-parser');
-const { body, validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
+const express = require('express');  //ES5 import express from node_modules, assign to express constant
+const app = express(); //create instance of express
+const environment = process.env.NODE_ENV || 'development';  //make the environment default to development except for hosting site
+const configuration = require('./knexfile')[environment];  //config file for knex access to database
+const database = require('knex')(configuration); //database for knex
+const bodyParser = require('body-parser'); //body-parser for posting
+
 
 app.use(bodyParser.json());
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Palette Picker';
+app.set('port', process.env.PORT || 3000);//default localhost to 3000 except for hosting
+app.locals.title = 'Palette Picker'; //data for dev prior to server creation
 
-app.use(express.static('public'));
+app.use(express.static('public'));//point express to the public folder for the app
 
 app.get('/', (request, response) => {
   // response.status(200).sendFile('public/index.html', { root: __dirname });
@@ -30,7 +29,7 @@ app.get('/', (request, response) => {
 //   { id: 'l3', project_id: 'p3', color_one: '#D73479', color_two: '#D73479', color_three: '#D73479', color_four: '#D73479', color_five: '#D73479' }
 // ];
 
-app.get('/api/v1/projects', (request, response) => {
+app.get('/api/v1/projects', (request, response) => { //retrieve project from database endpoint
   database('projects').select()
   .then((projects)=>{
     response.status(200).json(projects);
@@ -40,7 +39,7 @@ app.get('/api/v1/projects', (request, response) => {
   });
 });
 
-app.get('/api/v1/palettes', (request, response) => {
+app.get('/api/v1/palettes', (request, response) => { //retrieve palettes from database endpoint
   database('palettes').select()
     .then((palettes) => {
       response.status(200).json(palettes);
@@ -50,7 +49,7 @@ app.get('/api/v1/palettes', (request, response) => {
     });
 });
 
-app.post('/api/v1/projects', (request, response)=>{
+app.post('/api/v1/projects', (request, response)=>{ //post projects to database endpoint
 const project = request.body;
   database('projects').insert(project, 'id')
     .then(project => {
@@ -61,7 +60,7 @@ const project = request.body;
     });
 });
 
-app.post('/api/v1/palettes', (request, response) => {
+app.post('/api/v1/palettes', (request, response) => { //post palettes to database endpoint
   const palette = request.body;
 
   // for (let requiredParameter of ['palette_name', 'project_id', 'color_one', 'color_two', 'color_three', 'color_four', 'color_five']) {
@@ -81,7 +80,7 @@ app.post('/api/v1/palettes', (request, response) => {
     });
 });
 
-app.get('/api/v1/palettes/:id', (request, response) => {
+app.get('/api/v1/palettes/:id', (request, response) => {  //get dynamically defined palette from database
   database('palettes').where('id', request.params.id).select()
     .then(palettes => {
       if (palettes.length) {
@@ -97,6 +96,6 @@ app.get('/api/v1/palettes/:id', (request, response) => {
     });
 });
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => { //getting the server listening at the beginning
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
