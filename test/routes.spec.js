7d1d41dc9395441ce8e8781dd3f7f76261dab7e9
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-07 19:47:56 -0600

    Improve styling

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -12,21 +12,49 @@ chai.use(chaiHttp);
 describe("Test client endpoint ' / '", () => {
   it('base route should return html ', (done) => {
     chai.request(server)
-    .get('/')
-    .end((error, response)=>{
-      response.should.have.status(200); //always test the status code 
-      response.should.be.html; //should always test the type
-      //always test properties, if any(object)
-      done();
-    })
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200); //always test the status code 
+        response.should.be.html; //should always test the type
+        //always test properties, if any(object)
+        done();
+      })
   });
-  it('should ', (done) => {
+  it('A path that does not exist should return 404', (done) => {
     chai.request(server)
-    .get('/sad')  //sad path which does not exist
-    .end( (error, response)=>{
-      response.should.have.status(404);
-      done()
-    })
-    
+      .get('/sad')  //sad path which does not exist
+      .end((error, response) => {
+        response.should.have.status(404);
+        done()
+      })
   });
+});
+
+describe("API routes  ", () => {
+  beforeEach(done => {
+    knex.migrate.rollback().then(() => {
+      knex.migrate.latest().then(() => {
+        return knex.seed.run().then(() => {
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET /api/v1/projects', () => {
+    it.only('should  ', () => {
+      chai.request(server)
+        .get('/api/v1/projects/')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
+          response.body[0].should.have.property('project_name')
+        })
+
+    });
+  });
+
 });
